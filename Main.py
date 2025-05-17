255e6f8664e8b035cd9841ba6eb6f9bc4957fdf8
Dasha <dashakapusta666@gmail.com>
2025-05-17 20:51:20 +0300

    Корректировка текстур. Процедурное обновление текстур блоков.

--- Main.py
+++ Main.py
@@ -17,14 +17,31 @@ VEL = 5 #скорость персонажа
 window = pygame.display.set_mode((WIDTH, HEIGHT))
 
 bg_images = []
-for i in range(1, 6):
+for i in range(1, 4):
     bg_image = pygame.image.load(f"Background/Layer_{i}.png").convert_alpha()
     #bg_images.append(bg_image)
-    bg_images.append(pygame.transform.scale(bg_image, (WIDTH, HEIGHT)))
+    bg_images.append(pygame.transform.scale(bg_image, (HEIGHT * 1.7, HEIGHT)))
 
-def draw_bg():
-    for i in bg_images:
-        window.blit(i, (0, 0))
+def draw_bg(offset_x):
+    for idx, img in enumerate(bg_images):
+        if idx <= 0:
+            window.blit(img, (0, 0))
+        else:
+            speed = 0.2 * (idx)
+            width = img.get_width()
+            x_shift = offset_x * speed
+            
+            current_block = int(x_shift // width)
+            for i in range(current_block - 1, current_block + 2):
+                x_pos = (i * width) - x_shift
+                window.blit(img, (x_pos, 0))
+
+def check_block(x, y, objects):
+    for (obj_x, obj_y) in objects:
+        if obj_x == x and obj_y == y:
+            return True
+    return False
+
 
 def flip(sprites):
     return [pygame.transform.flip(sprite, True, False) for sprite in sprites]
@@ -53,8 +70,34 @@ def load_sprite_sheets(dir1, dir2, width, height, direction = False):
 
     return all_sprites
 
-def get_block(size):
-    path = join("Grass", "", "Grass_Top_Mid.png")
+def get_block(size, left, right, up, down):
+    blocks_surr = "Top_Mid"
+    if not up: 
+        if not down:
+            blocks_surr = "Tile"
+            if left and not right:
+                blocks_surr += "_Right"
+            elif right and not left:
+                blocks_surr += "_Left"
+            elif right and left:
+                blocks_surr += "_Mid"
+            elif not left and not right:
+                blocks_surr += "_Solo"
+        elif down:
+            if not left and not right:
+                blocks_surr = "Top_Solo"
+            elif right and left:
+                blocks_surr = "Top_Mid"
+            elif right and not left:
+                blocks_surr = "Top_Left"
+            elif not right and left:
+                blocks_surr = "Top_Right"
+                
+    elif up:
+        blocks_surr = "Mid_Mid"
+
+
+    path = join("Grass", "", "Grass_" + blocks_surr + ".png")
     image = pygame.image.load(path).convert_alpha()
     surface = pygame.Surface((size, size), pygame.SRCALPHA, 32)
     rect  = pygame.Rect(0, 0, size, size)
@@ -163,11 +206,15 @@ class Object(pygame.sprite.Sprite):
         win.blit(self.image, (self.rect.x - offset_x, self.rect.y))
 
 class Block(Object):
-    def __init__(self, x, y, size):
-        super().__init__(x, y, size, size)
-        block = get_block(size)
+    def __init__(self, x, y, size, is_block_left, is_block_right, is_block_top, is_block_bottom):
+        super().__init__(x, y, size, size)  # ← исправили здесь
+        self.x = x
+        self.y = y
+        self.size = size
+        block = get_block(size, is_block_left, is_block_right, is_block_top, is_block_bottom)
         self.image.blit(block, (0, 0))
         self.mask = pygame.mask.from_surface(self.image)
+
 
 
 def draw(window, player, objects, offset_x): #прорисовка всего происходящего 
@@ -198,16 +245,21 @@ def vertical_collision(player, objects, dy):
     return collided_objects
 
 def collide(player, objects, dx):
+    original_rect = player.rect.copy()
+    player.rect.y -= 5 
     player.move(dx, 0)
-    player.update()
     collided_object = None
+
     for obj in objects:
-        if pygame.sprite.collide_mask(player, obj):
+        if player.rect.colliderect(obj.rect):
             collided_object = obj
             break
+
     player.move(-dx, 0)
-    player.update()
+    player.rect = original_rect
+
     return collided_object
+
 
 def move(player, objects):
         
@@ -217,21 +269,48 @@ def move(player, objects):
     collide_left = collide(player, objects, -VEL)
     collide_right = collide(player, objects, VEL)
 
-    if keys[pygame.K_LEFT] and not collide_left:
+    if (keys[pygame.K_LEFT] or keys[pygame.K_a]) and not collide_left:
         player.move_left(VEL)  
-    if keys[pygame.K_RIGHT] and not collide_right:
+    if (keys[pygame.K_RIGHT] or keys[pygame.K_d]) and not collide_right:
         player.move_right(VEL)
 
     vertical_collision(player, objects, player.y_vel)
 
 def main(window):
-
     clock = pygame.time.Clock()
     block_size = 64
-
     player = Player(100, 100, 128, 128)
-    floor = [Block(i * block_size, HEIGHT - block_size, block_size) for i in range(-WIDTH // block_size, WIDTH * 2 // block_size)]
-    objects = [*floor, Block(0, HEIGHT - block_size * 2, block_size), Block(block_size * 3, HEIGHT - block_size * 4, block_size)]
+
+    # создаём заготовку всех координат блоков
+    floor_coords = []
+    for i in range(-WIDTH // block_size, WIDTH * 2 // block_size):
+        x = i * block_size
+        y1 = HEIGHT - block_size
+        y2 = HEIGHT
+        floor_coords.append((x, y1))
+        floor_coords.append((x, y2))
+        
+    other_coords = [
+        (block_size * 1, HEIGHT - block_size * 2),
+        (block_size * 2, HEIGHT - block_size * 2), # одиночный блок
+        (block_size * 5, HEIGHT - block_size * 4),  # начало платформы в 4 блока
+        (block_size * 6, HEIGHT - block_size * 4),
+        (block_size * 7, HEIGHT - block_size * 4),
+        (block_size * 3, HEIGHT - block_size * 4),
+        (block_size * 8, HEIGHT - block_size * 5)
+    ]
+    all_coords = floor_coords + other_coords
+
+    # создаём блоки с правильной проверкой соседей
+    objects = []
+    for (x, y) in all_coords:
+        is_block_left = check_block(x - block_size, y, all_coords)
+        is_block_right = check_block(x + block_size, y, all_coords)
+        is_block_top = check_block(x, y - block_size, all_coords)
+        is_block_bottom = check_block(x, y + block_size, all_coords)
+
+        block = Block(x, y, block_size, is_block_left, is_block_right, is_block_top, is_block_bottom)
+        objects.append(block)
 
     offset_x = 0
     scroll_area_width = 200
@@ -239,28 +318,28 @@ def main(window):
     run = True
     while run:
         clock.tick(FPS)
+        draw_bg(offset_x)
 
-        draw_bg()
-
-        for event in pygame.event.get(): #отслежинание закрытия окна
+        for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
                 break
 
             if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_SPACE and player.jump_count < 2:
+                if (event.key == pygame.K_SPACE or event.key == pygame.K_w or event.key == pygame.K_UP) and player.jump_count < 2:
                     player.jump()
-        
+
         player.loop(FPS)
         move(player, objects)
         draw(window, player, objects, offset_x)
-    
+
         if ((player.rect.right - offset_x >= WIDTH - scroll_area_width) and player.x_vel > 0) or (
             (player.rect.left - offset_x <= scroll_area_width) and player.x_vel < 0):
             offset_x += player.x_vel
 
     pygame.quit()
     quit()
+
 
 if __name__ == "__main__":
     main(window)
